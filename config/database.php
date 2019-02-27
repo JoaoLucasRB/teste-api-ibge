197d1aab70fe26450d05ee87fc634ea5a6cd978b
JoaoLucasRB <32493395+JoaoLucasRB@users.noreply.github.com>
2019-02-27 00:12:53 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,14 +1,12 @@
 <?php
 
 class Database {
-    private $usuario = "joao_admin";
-    private $senha = "34th53rT5NJ93uoR";
     public $conector;
 
     public function getConexao(){
         $this->conector = null;
         try{
-            $this->conector = new MongoDB\Client("mongodb+srv://joao_admin:34th53rT5NJ93uoR@jaotestenuvem-f6qrm.gcp.mongodb.net/test?retryWrites=true");
+            $this->conector = new MongoDB\Client("<string de conexao>");
         } catch (MongoDB\Driver\Exception $e) {
             echo "Erro de conexao:  ".$e->getMessage()."\n";
         }
